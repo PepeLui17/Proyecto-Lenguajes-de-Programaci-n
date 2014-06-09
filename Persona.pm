f707cf3234b7a2e23ffa838bb6446c57c5f349ae
José Monar <jl_mj_17@hotmail.com>
2014-06-09 02:05:51 -0500

    Se añadió la clase Persona y se modificó el archivo proyectoV1.pl para que guarde los datos en un array de Personas

---
+++ Persona.pm
@@ -0,0 +1,165 @@
+package Persona;
+  use strict; #Nos ponemos serios
+
+
+  ######################################################################
+  #Constructor de la clase
+  #
+
+  sub new {
+     my $this=shift; #Cogemos la clase que somos o una referencia a la clase (si soy un objeto)
+     my $class = ref($this) || $this; #Averiguo la clase a la que pertenezco
+
+     my $self={}; #Inicializamos la tabla hash que contendrá las var. de instancia (NOMBRE Y EDAD)
+     $self ->{NOMBRE} =undef ; #La clase Persona nace sin nombre  
+     $self ->{EDAD}   =0 ;     #La clase Persona se construye con 0 años
+	 $self ->{ESTATURA} =undef ;
+	 $self ->{PESO} =undef ;
+	 $self ->{CORREO} =undef ;
+	 $self ->{TELEFONO} =undef ;
+	 $self ->{ESTADO_CIVIL} =undef ;
+	 $self ->{UNIVERSIDAD} =undef ;
+	 $self ->{CARRERA} =undef ;
+	 $self ->{LUGAR_NACIMIENTO} =undef ;
+
+     bless $self, $class; #Perl nos tiene que dar el visto bueno (bendecirla)
+     return ($self); #Devolvemos la clase recién construida
+  }
+  
+  ######################################################################
+  #Métodos de acceso a los datos de la clase
+
+  #metodo para ver/cambiar el nombre
+  sub nombre{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será el nombre
+       $self->{NOMBRE}=shift if (@_);
+
+       #Devolvemos el nombre
+       return $self->{NOMBRE};
+  }
+
+  #metodo para ver/cambiar la edad
+  sub edad{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será la edad
+       $self->{EDAD}=shift if (@_);
+
+       #Devolvemos la edad
+       return $self->{EDAD};
+  }
+  
+  #metodo para ver/cambiar la estatura
+  sub estatura{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será la estatura
+       $self->{ESTATURA}=shift if (@_);
+
+       #Devolvemos la estatura
+       return $self->{ESTATURA};
+  }
+
+  #metodo para ver/cambiar el peso
+  sub peso{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será el peso
+       $self->{PESO}=shift if (@_);
+
+       #Devolvemos el peso
+       return $self->{PESO};
+  }
+  
+  #metodo para ver/cambiar el correo
+  sub correo{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será el correo
+       $self->{CORREO}=shift if (@_);
+
+       #Devolvemos el correo
+       return $self->{CORREO};
+  }
+  
+  #metodo para ver/cambiar el telefono
+  sub telefono{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será el telefono
+       $self->{TELEFONO}=shift if (@_);
+
+       #Devolvemos el telefono
+       return $self->{TELEFONO};
+  }
+  
+  #metodo para ver/cambiar el estado civil
+  sub estado_civil{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será el estado civil
+       $self->{ESTADO_CIVIL}=shift if (@_);
+
+       #Devolvemos el estado civil
+       return $self->{ESTADO_CIVIL};
+  }
+  
+  #metodo para ver/cambiar la universidad
+  sub universidad{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será la universidad
+       $self->{UNIVERSIDAD}=shift if (@_);
+
+       #Devolvemos la universidad
+       return $self->{UNIVERSIDAD};
+  }
+  
+  #metodo para ver/cambiar la carrera
+  sub carrera{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será la carrera
+       $self->{CARRERA}=shift if (@_);
+
+       #Devolvemos la carrera
+       return $self->{CARRERA};
+  }
+  
+  #metodo para ver/cambiar el lugar de nacimiento
+  sub lugar_nacimiento{
+       my $self=shift; #El primer parámetro de un metodo es la  clase
+ 
+       #Miramos si se le ha pasado algún parámetro, en cuyo caso será el lugar de nacimiento
+       $self->{LUGAR_NACIMIENTO}=shift if (@_);
+
+       #Devolvemos el lugar de nacimiento
+       return $self->{LUGAR_NACIMIENTO};
+  }
+  
+  ######################################################################
+  #Destructor
+  #
+
+  sub DESTROY {
+        my $self=shift; #El primer parámetro de un metodo es la  clase
+        delete ($self->{NOMBRE});  
+        delete ($self->{EDAD}); 
+		delete ($self->{ESTATURA}); 
+		delete ($self->{PESO}); 
+		delete ($self->{CORREO}); 
+		delete ($self->{TELEFONO}); 
+		delete ($self->{ESTADO_CIVIL}); 
+		delete ($self->{UNIVERSIDAD}); 
+		delete ($self->{CARRERA}); 
+		delete ($self->{LUGAR_NACIMIENTO}); 
+  }
+
+  #Fin
+  1;
+  
+  
+  #Codigo tomado de: http://flanagan.ugr.es/perl/objetos.htm
+  #Se le realizaron las modificaciones respectivas
--- proyectoV1.pl
+++ proyectoV1.pl
@@ -1,12 +1,5 @@
 use utf8;
-#use open 'locale'; 
-#use encoding;
-#use utf8;
-
-#use open ':locale'; 
- 
-#use open OUT => ':utf8';    # la salida será en utf8
-#use open ':std';            # y en los controladores estándares: STDIN, STDOUT, STDERR
+use Persona;
 
 $archivo=do { 
 	local $/; 
@@ -15,92 +8,147 @@ $archivo=do {
 @lineas=split(/\n/, $archivo);
 
 $i = 1;
+my @personas;
 
 foreach $linea (@lineas){
-	$linea =~ s/^\s+|\s+$//g; #TRIM
+	$linea =~ s/^\s+|\s+$//g; #TRIM		
+
 	
-	$temporal=$linea;
-	
-	if(length($temporal)>0){
-		print "Resumen persona # $i \n";
+	if(length($linea)>0){		
+		$persona= Persona->new(); #Creamos un objeto del tipo persona
 	}
+	
 
 	#Buscando nombres
-	if($temporal =~ /(nombre es|llamo) ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){		
-		print "Nombre: ".$2."\n";
+	if($linea =~ /([M|m]i nombre es|[M|m]e llamo) ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
+		$persona->nombre($2);
 	}
 	
 	#Buscando edades
-	if($temporal =~ /(t|T)engo ([0-9]{2}) años/){
-		print "Edad: ".$2."\n";
+	if($linea =~ /[t|T]engo ([0-9]{2}) [a|A]ños/){
+		$persona->edad($1);
 	}
 	
 	#Buscando pesos
-	#if($temporal =~ /(peso|peso alrededor de|peso es) ([0-9]{2,4})( kg| Kg|kg|Kg| libras)/){
-	if($temporal =~ /(peso|peso alrededor de|peso es) ([0-9]{2,4})(((\s)?(kg|Kg))| libras)/){	
-		print "Peso: ".$2.$3."\n";
+	if($linea =~ /(peso( alrededor de| es| aproximadamente)?)( de)? ([0-9]{2,4})(((\s)?(kg|Kg))| libras)/){	
+		$persona->peso($4.$5);
 	}
 	
 	#Buscando estaturas
-	if($temporal =~ /([m|M]ido|estatura es|estatura es de|[M|m]ido aproximadamente) ([0-9]{1}\.[0-9]{1,2}|[0-9]{3})( metros| m|m|cms| cms|cm| cm|,| )/){
-		print "Estatura: ".$2.$3."\n";
+	if($linea =~ /([m|M]ido( aproximadamente)?|estatura es( de)?) ([0-9]{1}\.[0-9]{1,2}|[0-9]{3})( metros| m|m|cms| cms|cm| cm|,| )/){
+		if($5=~ /,/){
+			$persona->estatura($4);
+		}
+		else{
+			$persona->estatura($4.$5);
+		}
 	}
 	
 	#Buscando correos
-	if($temporal =~ /([m|M]i correo es|[m|M]i correo electrónico es|[M|m]i correo) ([_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@(([a-z]+\.)+)[a-z]{2,4})/){
-		print "Correo: ".$2."\n";
+	if($linea =~ /([m|M]i( dirección( de)?)? (email|(correo( electrónico|(( de| en)? [e|E]spol))?)))( preferid[o|a])?( es)? ([_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@(([a-z]+\.)+)[a-z]{2,4})/){
+		 $persona->correo($11);
 	}	
 
-	#telefono,estado civil, lugar de nacimiento, Universidad, carrera.
 	#Buscando telefonos
-	if($temporal =~ /([C|c]elular|[T|t]eléfono|telefónico)( es( el)?)? ([0-9]{10})/){
-		print "Teléfono: ".$4."\n";
+	if($linea =~ /([C|c]elular|[T|t]eléfono|telefónico)( es( el)?)? ([0-9]{10})/){
+		$persona->telefono($4);
 	}
 
-	#Buscando Estado Civil
-	if($temporal =~ /(solter[o|a]|casad[o|a]|divorciad[o|a]|viud[o|a])/){
-		print "Estado Civil: ".$1."\n";
+	#Buscando estado civil
+	if($linea =~ /(solter[o|a]|casad[o|a]|divorciad[o|a]|viud[o|a])/){
+		 $persona->estado_civil($1);
 	}
 
 	#Buscando Universidad
-	#if($temporal =~/([E|e]spol)/){
-	if($temporal =~/([E|e]studio en|[S|s]oy estudiante de)( la)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
-		$universidad = $3;
-		if($universidad =~ /([I|i]ngeniería)/){
-			  #print "";
+	if($linea =~/(([E|e]studio( en)?)|[S|s]oy estudiante de)( la)?( universidad)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
+	#if($linea =~/(([E|e]studio( en)?)|[S|s]oy estudiante de)( la)?( universidad)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)( en)?( la)?( universidad)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
+	 #if($linea =~/(([E|e]studio( en)?)|[S|s]oy estudiante de)( la)?( universidad)?([A-Za-zÁÉÍÓÚáéíóúñ\s]+)?( en)?( la)?( universidad)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
+		$universidad = $6;
+		#$universidad = $10;
+		
+		#print $6."\n";
+		if($universidad =~ /([I|i]ngeniería|Ing(\.)?)/){
+			  #print $universidad."\n";
+			  #No hacer nada
 		}
 		else{
-			print "Universidad: ".$universidad."\n";
+			$persona->universidad($universidad);
 		}
 	}
-
+	
+	#Buscando Carrera
+	if($linea =~/carrera( de| es)( estudio es)?(( [I|i]ng[\.]?)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+))(,|.)/){
+		$persona->carrera($3);
+	}
+	else{
+		if($linea =~/([E|e]studio|[S|s]oy estudiante de)(( [I|i]ng[\.]?)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+))(,|.)/){
+			$carrera=$2;
+			$comp=$1."".$2;
+			if($comp =~ /([E|e]studio en la)|estudiante de la/){
+				#No hacer nada
+			}
+			else{
+				$persona->carrera($carrera);				
+			}
+		}
+	}
+	
 	#Buscando Lugar de nacimiento
 	#if($temporal =~/([N|n]ací en)( la( ciudad de)?)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.| el)/){
 	#if($temporal =~/([N|n]ací)? en( la( ciudad de)?)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
-	if($temporal =~/([N|n]ací en)( la( ciudad de)?)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
-		print "Lugar de nacimiento: ".$4."\n";
+	if($linea =~/([N|n]ací en)( la( ciudad de)?)? ([A-Za-zÁÉÍÓÚáéíóúñ\s]+)(,|.)/){
+		#print "Lugar de nacimiento: ".$4."\n";
+		$persona->lugar_nacimiento($4);
 	}
 	
 	#Buscando domicilio
 	
 	#Buscando Carrera
-	if($temporal =~/([a-zA-Z]+)([a-zA-Z]*) ([C|c]iencias )([a-zA-Z]+)/){
-		print "Carrera: ".$2.$3.$4."\n";
-	}
+	#if($linea =~/([a-zA-Z]+)([a-zA-Z]*) ([C|c]iencias )([a-zA-Z]+)/){
+	#	#print "Carrera: ".$2.$3.$4."\n";
+	#	$persona->carrera($2.$3.$4);
+	#}
 
 	#Buscando Especializacion
-	if($temporal =~/([S|s]istemas )([a-zA-Z]*) ([a-zA-Z]+)/){
-			print "Especialidad: ".$1.$2." ".$3."\n";
+	if($linea =~/([S|s]istemas )([a-zA-Z]*) ([a-zA-Z]+)/){
+			#print "Especialidad: ".$1.$2." ".$3."\n";
 	}
 
+	
+	#Hobbies, identificacion, fecha nacimiento, especializacion, dirección
 	#Buscando Fecha de Nacimiento
 	#if($temporal=~/([N|n]ac[?] )([a-z]{2}*) ([a-z]*) ([a-z]*) (([0-9]{2}\/[0-9]{2}\/[0-9]{2,4})|(([0-9]{2}) (de )([a-zA-Z])( de )([0-9]{2,4})))/){
 	#	print "Fecha de Nacimiento: ".$2."\n";
 	#}
 
-	print "\n";
-	$i++;
+	push @personas, $persona;
+	#print "\n";
+	#$i++;
 }
+
+#Mostrando información de cada persona
+foreach $person (@personas){
+	print "Resumen persona # ".$i."\n";
+	print "Nombre: ".$person->nombre."\n";
+	#print "Edad: ".$person->edad."\n";
+	#print "Peso: ".$person->peso."\n";
+	#print "Estatura: ".$person->estatura."\n";
+	#print "Correo: ".$person->correo."\n";
+	#print "Telefono: ".$person->telefono."\n";
+	#print "Estado civil: ".$person->estado_civil."\n";
+	print "Universidad: ".$person->universidad."\n";
+	print "Carrera: ".$person->carrera."\n";
+	#print "Lugar nacimiento: ".$person->lugar_nacimiento."\n";
+	
+	#if($person->edad <21){
+	#	print $person->nombre." tiene ".$person->edad." años, puede ir a la fiesta \n";
+	#}
+	
+	print "\n";
+	$i++;  
+  }
+
+#print "\nSuma de edades: ".$sum;
 
 
 #cd C:\Users\José Luis\Documents\Perl files
